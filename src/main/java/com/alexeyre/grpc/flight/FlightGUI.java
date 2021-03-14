0db69d7d6f50a46084bbc5a0f8a0896d08d556db
Alex Eyre <eyrealex97@gmail.com>
2021-03-14 01:05:44 +0000

    implemented client side streaming and server side streaming for GUI, runs perfectly on server, client and GUI

--- src/main/java/com/alexeyre/grpc/flight/FlightGUI.java
+++ src/main/java/com/alexeyre/grpc/flight/FlightGUI.java
@@ -20,6 +20,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 import com.alexeyre.grpc.flight.FlightServiceGrpc.FlightServiceBlockingStub;
 import com.alexeyre.grpc.flight.FlightServiceGrpc.FlightServiceStub;
@@ -27,6 +28,7 @@ import com.alexeyre.grpc.flight.FlightServiceGrpc.FlightServiceStub;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 
 public class FlightGUI {
 
@@ -34,7 +36,8 @@ public class FlightGUI {
 	private static FlightServiceStub asyncStub;
 	private static ServiceInfo serviceInfo;
 	private JFrame jFrame;
-	private JTextArea jTextArea;
+	private JTextArea jTextArea, jTextArea2;
+	private JTextField textNumber1, textNumber2, textNumber3, textNumber4;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -126,11 +129,15 @@ public class FlightGUI {
 
 		jFrame = new JFrame();
 		jFrame.setTitle("Flight Client - Service Controller");
-		jFrame.setBounds(100, 100, 500, 300);
+		jFrame.setBounds(400, 400, 500, 450);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		BoxLayout bl = new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS);
 		jFrame.getContentPane().setLayout(bl);
+
+		// ******************************************
+		// SETTING THE JPANEL FOR THE FIRST SERVICE
+		// ******************************************
 
 		JPanel jPanel1 = new JPanel();
 		jFrame.getContentPane().add(jPanel1);
@@ -153,7 +160,7 @@ public class FlightGUI {
 					while (response.hasNext()) {
 						ListResponse temp = response.next();
 						jTextArea.append("\nLocation: " + temp.getResult());
-					
+
 						System.out.println("Location: " + temp.getResult());
 					}
 
@@ -171,9 +178,115 @@ public class FlightGUI {
 		jTextArea.setWrapStyleWord(true);
 
 		JScrollPane scrollPane = new JScrollPane(jTextArea);
-
 		// textResponse.setSize(new Dimension(15, 30));
 		jPanel1.add(scrollPane);
+
+		// ******************************************
+		// SETTING THE JPANEL FOR THE SECOND SERVICE
+		// ******************************************
+
+		JPanel jPanel2 = new JPanel();
+		jFrame.getContentPane().add(jPanel2);
+		jPanel2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		JLabel JLabel2 = new JLabel("Departure Destination: ");
+		jPanel2.add(JLabel2);
+		textNumber1 = new JTextField();
+		jPanel2.add(textNumber1);
+		textNumber1.setColumns(10);
+
+		JLabel JLabel3 = new JLabel("Departure Date: ");
+		jPanel2.add(JLabel3);
+		textNumber2 = new JTextField();
+		jPanel2.add(textNumber2);
+		textNumber2.setColumns(10);
+
+		JLabel JLabel4 = new JLabel("Arrival Destination: ");
+		jPanel2.add(JLabel4);
+		textNumber3 = new JTextField();
+		jPanel2.add(textNumber3);
+		textNumber3.setColumns(10);
+
+		JLabel JLabel5 = new JLabel("Arrival Date: ");
+		jPanel2.add(JLabel5);
+		textNumber4 = new JTextField();
+		jPanel2.add(textNumber4);
+		textNumber4.setColumns(10);
+
+		JButton btnBooking = new JButton("Book");
+		btnBooking.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				String val1 = textNumber1.getText();
+				String val2 = textNumber2.getText();
+				String val3 = textNumber3.getText();
+				String val4 = textNumber4.getText();
+
+				StreamObserver<BookingResponse> response = new StreamObserver<BookingResponse>() {
+
+					@Override
+					public void onNext(BookingResponse value) {
+						jTextArea2.append("TESTING" + value.getDepart() + value.getDepartDate() + value.getArrival() + value.getArrivalDate());
+						System.out.println("TESTING" + value.getDepart() + value.getDepartDate() + value.getArrival() + value.getArrivalDate());
+
+					}
+
+					@Override
+					public void onError(Throwable t) {
+						t.printStackTrace();
+
+					}
+
+					@Override
+					public void onCompleted() {
+						System.out.print("Booking Completed!!");
+
+					}
+
+				};
+
+				StreamObserver<BookingRequest> request = asyncStub.flightBooking(response);
+				try {
+
+					request.onNext(BookingRequest.newBuilder().setValue(val1).build());
+					Thread.sleep(500);
+
+					request.onNext(BookingRequest.newBuilder().setValue(val2).build());
+					Thread.sleep(500);
+
+					request.onNext(BookingRequest.newBuilder().setValue(val3).build());
+					Thread.sleep(500);
+
+					request.onNext(BookingRequest.newBuilder().setValue(val4).build());
+					Thread.sleep(500);
+
+					// Mark the end of requests
+					request.onCompleted();
+
+					Thread.sleep(3000);
+
+				} catch (RuntimeException t) {
+					t.printStackTrace();
+				} catch (InterruptedException t) {
+					t.printStackTrace();
+				}
+			}
+		});
+
+		jPanel2.add(btnBooking);
+
+		jTextArea2 = new JTextArea(3, 20);
+		jTextArea2.setLineWrap(true);
+		jTextArea2.setWrapStyleWord(true);
+
+		JScrollPane scrollPane2 = new JScrollPane(jTextArea2);
+		jPanel2.add(scrollPane2);
+
+		JPanel jPanel3 = new JPanel();
+		jFrame.getContentPane().add(jPanel3);
+
+		JPanel jPanel4 = new JPanel();
+		jFrame.getContentPane().add(jPanel4);
 
 	}
 
