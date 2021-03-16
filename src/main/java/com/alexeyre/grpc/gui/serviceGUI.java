35164dc19ff79598c4cd4886d5499d76f45ae0bf
Alex Eyre <eyrealex97@gmail.com>
2021-03-16 13:21:27 +0000

    made alot of changes to GUI, refrectored naming conventions for files, finished flight service for two panels, working on the third and fourth

--- src/main/java/com/alexeyre/grpc/gui/serviceGUI.java
+++ src/main/java/com/alexeyre/grpc/gui/serviceGUI.java
@@ -1,9 +1,11 @@
 package com.alexeyre.grpc.gui;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -20,8 +22,10 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 import com.alexeyre.grpc.flight.BookingRequest;
 import com.alexeyre.grpc.flight.BookingResponse;
@@ -41,8 +45,9 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
+import java.awt.Color;
 
-public class FlightGUI {
+public class serviceGUI {
 
 	private static FlightServiceBlockingStub blockingStub1;
 	private static FlightServiceStub asyncStub1;
@@ -52,14 +57,16 @@ public class FlightGUI {
 	private static RentalServiceStub asyncStub3;
 	private static ServiceInfo serviceInfo;
 	private JFrame jFrame;
-	private JTextArea jTextArea, jTextArea2;
+	private JTabbedPane tabbedPane;
+	private JTextArea jTextArea, jTextArea2, jTextArea3;
+	private JTextField jTextField;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					FlightGUI flightGUI = new FlightGUI();
-					flightGUI.jFrame.setVisible(true);
+					serviceGUI serviceGUI = new serviceGUI();
+					serviceGUI.jFrame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -68,7 +75,7 @@ public class FlightGUI {
 
 	}
 
-	public FlightGUI() {
+	public serviceGUI() {
 
 		String service_type = "_http._tcp.local.";
 		discoverFlightService(service_type);
@@ -86,8 +93,8 @@ public class FlightGUI {
 		blockingStub3 = RentalServiceGrpc.newBlockingStub(channel);
 		asyncStub3 = RentalServiceGrpc.newStub(channel);
 
-		getList();
-		flightBooking();
+		initGUI();
+		ServiceController();
 
 	}
 
@@ -144,78 +151,162 @@ public class FlightGUI {
 
 	}
 
-	private void getList() {
-
+	public void initGUI() {
 		jFrame = new JFrame();
-		jFrame.setTitle("Flight Client - Service Controller");
-		jFrame.setBounds(400, 400, 500, 450);
+		jFrame.setTitle("Holiday Booking App");
+		jFrame.setBounds(400, 400, 600, 500);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		BoxLayout bl = new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS);
-		jFrame.getContentPane().setLayout(bl);
+	}
 
-		JPanel jPanel1 = new JPanel();
-		jFrame.getContentPane().add(jPanel1);
-		jPanel1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+	private void ServiceController() {
+
+		// Create JTabbedPane object
+		JTabbedPane tabbedPane = new JTabbedPane();
+
+		// Initialize the tabs
+		JPanel tabPanel1 = new JPanel(false);
+		JPanel tabPanel2 = new JPanel(false);
+		JPanel tabPanel3 = new JPanel(false);
+
+		// panels for tab 2
+		JPanel p4 = new JPanel();
+		JPanel p5 = new JPanel();
+		JPanel p6 = new JPanel();
+		// panels for tab 3
+		JPanel p7 = new JPanel();
+		JPanel p8 = new JPanel();
+		JPanel p9 = new JPanel();
+
+		// Setting the tabs to a box layout
+		BoxLayout b1 = new BoxLayout(tabPanel1, BoxLayout.Y_AXIS);
+		BoxLayout b2 = new BoxLayout(tabPanel2, BoxLayout.Y_AXIS);
+		BoxLayout b3 = new BoxLayout(tabPanel3, BoxLayout.Y_AXIS);
+
+		// Configurations for tab 1
+
+		// Panel 1
+		JPanel list_flights_panel = new JPanel();
+		list_flights_panel.setBackground(new Color(128, 128, 128));
+
+		// Panel 2
+		JPanel booking_flight_panel = new JPanel();
+		booking_flight_panel.setBackground(Color.LIGHT_GRAY);
+
+		// Panel 3
+		JPanel passengers_flight_panel = new JPanel();
+		passengers_flight_panel.setBackground(new Color(128, 128, 128));
+
+		// Adding tab 1 Frame
+		tabPanel1.setLayout(b1);
+		tabbedPane.addTab("Flight", null, tabPanel1, "Tab 1 tooltip");
+		tabbedPane.setMnemonicAt(0, KeyEvent.VK_1);
+
+		// Adding tab 2 Frame
+		tabPanel2.setLayout(b2);
+		tabbedPane.addTab("Hotel", null, tabPanel2, "Tab 2 tooltip");
+		tabbedPane.setMnemonicAt(0, KeyEvent.VK_2);
+
+		// Adding tab 3 Frame
+		tabPanel3.setLayout(b3);
+		tabbedPane.addTab("Rental", null, tabPanel3, "Tab 3 tooltip");
+		tabbedPane.setMnemonicAt(0, KeyEvent.VK_3);
+
+		// Formatting panels for tab 1
+		list_flights_panel.setAlignmentX(0.6f);
+		list_flights_panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 15));
+		booking_flight_panel.setAlignmentX(0.6f);
+		booking_flight_panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 15));
+		passengers_flight_panel.setAlignmentX(0.6f);
+		passengers_flight_panel.setLayout(new FlowLayout(FlowLayout.CENTER, 10, 15));
+
+		// Formatting panels for tab 2
+		p4.setAlignmentX(0.6f);
+		p4.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		p5.setAlignmentX(0.6f);
+		p5.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		p6.setAlignmentX(0.6f);
+		p6.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		// Formatting panels for tab 3
+		p7.setAlignmentX(0.6f);
+		p7.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		p8.setAlignmentX(0.6f);
+		p8.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		p9.setAlignmentX(0.6f);
+		p9.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		// Adding panel 1 to tab 1
+		tabPanel1.add(list_flights_panel);
 
 		JLabel JLabel1 = new JLabel("Get list of Flight Locations: ");
-		jPanel1.add(JLabel1);
+		list_flights_panel.add(JLabel1);
 
 		JButton btnList = new JButton("Destinations");
+		list_flights_panel.add(btnList);
+
+		jTextArea = new JTextArea(3, 15);
+		list_flights_panel.add(jTextArea);
+		jTextArea.setLineWrap(true);
+		jTextArea.setWrapStyleWord(true);
+
+		// creating action when btnList is clicked
 		btnList.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
+				// Setting each value in flightList requests to pre determined values for server
+				// side streaming
 				ListRequest req = ListRequest.newBuilder().setLocation1("London").setLocation2("Paris")
 						.setLocation3("Berlin").setLocation4("Madrid").build();
 
+				// Iterate through the response and print off all pre determined values at once
+				// using the blockingStub
 				try {
 					Iterator<ListResponse> response = blockingStub1.flightList(req);
 					System.out.println("\nGetting list of holdiay destinations from the GUI");
 
+					// while there is another response.. print off the values to the console and
+					// text area
 					while (response.hasNext()) {
 						ListResponse temp = response.next();
-						jTextArea.append("\nLocation: " + temp.getResult());
+						jTextArea.append("Location: " + temp.getResult() + "\n");
 
 						System.out.println("Location: " + temp.getResult());
-					}
+					} // end while
 
 				} catch (StatusRuntimeException f) {
 					f.printStackTrace();
-				}
+				} // end catch
 
-			}
-		});
+			}// end action performed for btnList
+		}); // end action listerner for btnList
 
-		jPanel1.add(btnList);
+		// Adding panel 2 to tab 1
+		tabPanel1.add(booking_flight_panel);
 
-		jTextArea = new JTextArea(3, 20);
-		jTextArea.setLineWrap(true);
-		jTextArea.setWrapStyleWord(true);
-
-		JScrollPane scrollPane = new JScrollPane(jTextArea);
-		// textResponse.setSize(new Dimension(15, 30));
-		jPanel1.add(scrollPane);
-
-	}
-
-	private void flightBooking() {
-
-		JPanel jPanel2 = new JPanel();
-		jFrame.getContentPane().add(jPanel2);
-		jPanel2.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		JTextField jTextField;
+		// Configuring buttons, labels and text fields for booking flight panel
 		JLabel JLabel2 = new JLabel("Location/Date: ");
-		jPanel2.add(JLabel2);
+		booking_flight_panel.add(JLabel2);
 		jTextField = new JTextField();
-		jPanel2.add(jTextField);
+		booking_flight_panel.add(jTextField);
 		jTextField.setColumns(10);
 
 		JButton btnSubmit = new JButton("Submit");
+		booking_flight_panel.add(btnSubmit);
+
+		jTextArea2 = new JTextArea(3, 15);
+		booking_flight_panel.add(jTextArea2);
+		jTextArea2.setLineWrap(true);
+		jTextArea2.setWrapStyleWord(true);
+
+		// Initializing array list to be used for setting values
 		ArrayList<String> list = new ArrayList();
 
+		// creating action when btnSubmit is clicked
 		btnSubmit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
+				// Iterating through the stream of responses for flightBooking
 				StreamObserver<BookingResponse> responseObserver = new StreamObserver<BookingResponse>() {
 
 					@Override
@@ -252,39 +343,37 @@ public class FlightGUI {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-
-					}
+					}// end on next
 
 					@Override
 					public void onError(Throwable t) {
 						t.printStackTrace();
-
-					}
+					}// end on error
 
 					@Override
 					public void onCompleted() {
 						System.out.println("\nCompleted stream");
-
-					}
-
-				};
+					}// end on completed
+				}; // end Iterating through the stream of responses for flightBooking
 
 				String value = jTextField.getText();
-
 				if (list.add(value)) {
 					jTextField.setText("");
-				}
+				} // end if for removing text from text fields
 
+				// Iterating through the stream of requests for flightBooking
 				StreamObserver<BookingRequest> requestObserver = asyncStub1.flightBooking(responseObserver);
 
+				// if the list of requests is greater than 3 do ...
 				if (list.size() > 3) {
 
+					// loop through the list size and for each position set the value in the request
+					// this prevents overriding values for each user input
 					try {
-
 						for (int i = 0; i < list.size(); i++) {
 							requestObserver.onNext(BookingRequest.newBuilder().setValue(list.get(i)).build());
 							Thread.sleep(500);
-						}
+						} // end for loop
 
 						// Mark the end of requests
 						requestObserver.onCompleted();
@@ -298,25 +387,49 @@ public class FlightGUI {
 						e1.printStackTrace();
 					}
 
-				}
+				} // end if the list of requests is greater than 3 do ...
 
-			}
-		});
+			}// end btnSubmit action performed
+		}); // end btnSubmit action listener
 
-		jPanel2.add(btnSubmit);
+		// Adding panel 2 to tab 1
+		tabPanel1.add(passengers_flight_panel);
 
-		jTextArea2 = new JTextArea(3, 20);
-		jTextArea2.setLineWrap(true);
-		jTextArea2.setWrapStyleWord(true);
+		// Configuring buttons, labels and text fields for booking flight panel
+		JLabel JLabel3 = new JLabel("Number of passengers: ");
+		passengers_flight_panel.add(JLabel3);
+		jTextField = new JTextField();
+		passengers_flight_panel.add(jTextField);
+		jTextField.setColumns(10);
 
-		JScrollPane scrollPane2 = new JScrollPane(jTextArea2);
-		jPanel2.add(scrollPane2);
+		JButton btnSubmit2 = new JButton("Submit");
+		passengers_flight_panel.add(btnSubmit2);
 
-		JPanel jPanel3 = new JPanel();
-		jFrame.getContentPane().add(jPanel3);
+		jTextArea3 = new JTextArea(3, 10);
+		passengers_flight_panel.add(jTextArea3);
+		jTextArea3.setLineWrap(true);
+		jTextArea3.setWrapStyleWord(true);
 
-		JPanel jPanel4 = new JPanel();
-		jFrame.getContentPane().add(jPanel4);
+		// creating action when btnSubmit is clicked
+		btnSubmit2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+			}// end btnSubmit2 action performed
+
+		});// end btnSubmit2 action listener
+
+		// adding the sections to the second tab
+		tabPanel2.add(p4);
+		tabPanel2.add(p5);
+		tabPanel2.add(p6);
+
+		// adding the sections to the third tab
+		tabPanel3.add(p7);
+		tabPanel3.add(p8);
+		tabPanel3.add(p9);
+
+		// show the frame border
+		jFrame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
 
 	}
 
